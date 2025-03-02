86a0edc43a77a8f67bd5bb33e8e4bdc2ef8abcf0
JW-Ahn0 <jun.woo.an0@gmail.com>
2025-03-02 22:03:09 +0900

    feat: 디자인 작업수정_0302

--- src/FilePage.jsx
+++ src/FilePage.jsx
@@ -8,11 +8,14 @@ const FilePage = () => {
   const fileName = params.fileName;
   const [imageUrl, setImageUrl] = useState("");
   const [videoUrl, setVideoUrl] = useState("");
+  const [selectedVideoUrl, setSelectedVideoUrl] = useState("");
   const [error, setError] = useState("");
 
   useEffect(() => {
     const imageRef = ref(storage, fileName + "/image.jpg");
     const videoRef = ref(storage, fileName + "/video.mp4");
+    const selectedvideoRef = ref(storage, fileName + "/selectedvideo.mp4");
+
     getDownloadURL(imageRef)
       .then((url) => {
         setImageUrl(url);
@@ -22,6 +25,12 @@ const FilePage = () => {
     getDownloadURL(videoRef)
       .then((url) => {
         setVideoUrl(url);
+      })
+      .catch((error) => setError(error));
+
+    getDownloadURL(selectedvideoRef)
+      .then((url) => {
+        setSelectedVideoUrl(url);
       })
       .catch((error) => setError(error));
   }, [fileName]);
@@ -51,6 +60,8 @@ const FilePage = () => {
             ? "/video.mp4"
             : type === "image"
             ? "/image.jpg"
+            : type === "selectedvideo"
+            ? "/selectedvideo.mp4"
             : "");
 
         document.body.appendChild(a);
@@ -64,46 +75,96 @@ const FilePage = () => {
   };
 
   return (
-    <div className="container">
-      <img className="logo" src="/downloadPage_logo.png" alt="fiftypagelogo" />
+    <div className="Container">
+      <img className="HeaderLogo" src="/img/Photobook.png" alt="HeaderLogo" />
+      <img className="Logo" src="/img/50_Page.png" alt="Logo" />
       {imageUrl ? (
-        <img className="requestedPhoto" src={imageUrl} alt="requestedPhoto" />
+        <img className="Requested-Photo" src={imageUrl} alt="RequestedPhoto" />
       ) : (
         "Loading.."
       )}
       {error ? error : <></>}
-      <div className="contentContainer">
-        <p>사파리 혹은 크롬에서 다운로드 가능합니다.</p>
-        <div>
-          <button
-            className="downloadContainer"
-            onClick={() => downLoadFile(imageUrl, "image")}
-          >
-            <div className="downloadText">
-              <span>사진 다운로드</span>
-              <span>DOWNLOAD PHOTO</span>
-            </div>
-            <img className="download" src="/download.png" alt="download" />
-          </button>
-          <button
-            className="downloadContainer"
-            onClick={() => downLoadFile(videoUrl, "video")}
-          >
-            <div className="downloadText">
-              <span>동영상 다운로드</span>
-              <span>DOWNLOAD VIDEO</span>
-            </div>
-            <img className="download" src="/download.png" alt="download" />
-          </button>
+      <div className="Content-Container">
+        {/* 안내문구 컨테이너 */}
+        <div className="Info-Container">
+          <div className="Info-Text">
+            <p className="Info-Text-Title">
+              사파리 혹은 크롬에서 다운로드 가능합니다.⭐
+            </p>
+          </div>
+          <div className="Info-Text">
+            <span className="Info-Text-Subtitle">
+              Download in Safari or Chrome browser⭐
+            </span>
+          </div>
         </div>
-        <div className="instaContainer">
-          50page.official
+        {/* 버튼 컨테이너 */}
+        <div className="Button-Box-Container">
+          {/* 사진 다운로드 */}
+          <div className="Button-Box">
+            <div className="Button-Box-Text">
+              <p className="Button-Box-Text-Title">사진 다운로드</p>
+              <span className="Button-Box-Text-Subtitle">Download photo</span>
+            </div>
+            <span className="Dvider"></span>
+            <div className="Button-Box-Icon">
+              <button onClick={() => downLoadFile(imageUrl, "image")}>
+                <img
+                  className="Download-Icon"
+                  src="/img/Download.svg"
+                  alt="Download"
+                />
+              </button>
+            </div>
+          </div>
+          {/* 선택한 영상 다운로드 */}
+          <div className="Button-Box">
+            <div className="Button-Box-Text">
+              <p className="Button-Box-Text-Title">선택한 영상 다운로드</p>
+              <span className="Button-Box-Text-Subtitle">
+                Download select video
+              </span>
+            </div>
+            <span className="Dvider"></span>
+            <div className="Button-Box-Icon">
+              <button
+                onClick={() => downLoadFile(selectedVideoUrl, "selectedvideo")}
+              >
+                <img
+                  className="Download-Icon"
+                  src="/img/Download.svg"
+                  alt="Download"
+                />
+              </button>
+            </div>
+          </div>
+          {/* 영상 다운로드 */}
+          <div className="Button-Box">
+            <div className="Button-Box-Text">
+              <p className="Button-Box-Text-Title">영상 다운로드</p>
+              <span className="Button-Box-Text-Subtitle">Download video</span>
+            </div>
+            <span className="Dvider"></span>
+            <div className="Button-Box-Icon">
+              <button onClick={() => downLoadFile(videoUrl, "video")}>
+                <img
+                  className="Download-Icon"
+                  src="/img/Download.svg"
+                  alt="Download"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* 배너 컨테이너 */}
+        <div className="Banner-Container">
+          <img src="/img/Event.png" alt="BannerText" />
           <a
             href="https://www.instagram.com/50page.official"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img className="instaImage" src="/instagram.png" alt="instagram" />
+            <img src="/img/Bottom_Banner.png" alt="BottomBanner" />
           </a>
         </div>
       </div>
